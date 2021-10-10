b55c889bba728fb48e20c852c7dbad37b52f6782
Jethro <estangkij@gmail.com>
2021-10-10 12:53:24 +0800

    Add loop command

---
+++ commands/music/loop.js
@@ -0,0 +1,40 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class LoopCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'loop',
+      aliases: [`repeat`],
+      group: 'music',
+      memberName: 'loop',
+      guildOnly: true,
+      description: 'Loop the currently playing song!'
+    });
+  }
+
+  run(message) {
+    if (!message.guild.musicData.isPlaying) {
+      message.say(':x: There is no song playing right now!');
+      return;
+    } else if (
+      message.member.voice.channel.id !== message.guild.me.voice.channel.id
+    ) {
+      message.say(
+        `You must be in the same voice channel as the bot in order to use that!`
+      );
+      return;
+    }
+
+    if (message.guild.musicData.loopSong) {
+      message.guild.musicData.loopSong = false;
+      message.channel.send(
+        `**${message.guild.musicData.nowPlaying.title}** is no longer playing on repeat :repeat: `
+      );
+    } else {
+      message.guild.musicData.loopSong = true;
+      message.channel.send(
+        `**${message.guild.musicData.nowPlaying.title}** is now playing on repeat :repeat: `
+      );
+    }
+  }
+};
